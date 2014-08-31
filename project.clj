1e1bcb90284b307b1353081b70a05fb2a0b0962d
David Thomas Hume <dth@dthu.me>
2014-08-31 18:46:44 +0100

    Move to release of data.set

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
   :dependencies [[clj-tuple "0.1.5"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/data.finger-tree "0.0.2"]
-                 [org.dthume/data.set "0.1.0-SNAPSHOT"]]
+                 [org.dthume/data.set "0.1.0"]]
 
   :javac-options ["-target" "1.6" "-source" "1.6"]
 
